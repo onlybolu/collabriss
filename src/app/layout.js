000638a92e92326cea8a748b2bf70dba70cd6790
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-09-23 23:05:00 -0700

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Collabriss",
+  description: "The all-in-one commerce and business management tool for entrepreneurs. Sell online, manage inventory, and grow your customer base with ease.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
